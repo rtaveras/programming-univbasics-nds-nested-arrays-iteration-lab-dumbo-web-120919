1b958a1b0e83956cb6cce7bf2106ec76222711c8
Riaan Taveras <riaantaveras@gmail.com>
2019-12-09 00:43:02 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,36 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  new_array = ["I love #{src[0][0]} and #{src[0][1]} on my pizza", "I love #{src[1][0]} and #{src[1][1]} on my pizza", "I love #{src[2][0]} and #{src[2][1]} on my pizza"]
+  return new_array
 end
+
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  num = 0
+  new_array = []
+  while num < src.length
+  new_array << src[num].max
+  num += 1
+ end  
+ return new_array
 end
 
-def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
-end
+
+ 
+def total_even_pairs(array)
+  even_sum = 0
+  num1 = 0
+  while num1 < array.length
+   num2 = 0
+    while num2 < array[num1].length
+     even_sum += array[num1][num2]
+     num2 += 1
+    end
+   num1 += 1 
+  end
+  return even_sum
+end 
+ 
+
+
+
+
